82e18d10dca5ae1244b28e0397c725d403199cdc
Yuku Takahashi <yuku@flywheel.jp>
2019-11-17 14:57:10 +0900

    Implement proxy endpoint

--- index.ts
+++ index.ts
@@ -1,3 +1,5 @@
+import path from "path"
+
 import express from "express"
 import { Storage } from "@google-cloud/storage"
 
@@ -16,6 +18,21 @@ app.set("views", "./views")
 const storage = new Storage()
 const bucket = storage.bucket(bucketName)
 
+interface GoogleError {
+  code: number
+  message: string
+}
+
+// eslint-disable-next-line @typescript-eslint/no-explicit-any
+const isGoogleError = (e: any): e is GoogleError => {
+  return (
+    e != null && typeof e.code === "number" && typeof e.message === "string"
+  )
+}
+
+/**
+ * Mouseion index page. List all documents.
+ */
 app.get("/", async (_, responseWriter) => {
   const response = await bucket.getFiles({
     autoPaginate: false,
@@ -30,14 +47,29 @@ app.get("/", async (_, responseWriter) => {
   })
 })
 
-app.get("/sample", async (_, res) => {
-  const file = bucket.file("sample.html")
+/**
+ * Proxy for individual files.
+ */
+app.get("/*", async (req, res) => {
+  const name = req.url.substring(1)
+
+  let file = bucket.file(name)
+  const [exists] = await file.exists()
+  if (!exists) {
+    // If there isn't a file with the given name, treat it as a directory.
+    file = bucket.file(path.join(name, "index.html"))
+  }
 
   file
     .createReadStream()
-    .on("error", () => {
-      res.status(404)
-      res.send("Not Found")
+    .on("error", e => {
+      if (isGoogleError(e)) {
+        res.status(e.code)
+        res.send(e.message)
+      } else {
+        res.status(500)
+        res.send("Internal server error")
+      }
     })
     .on("end", () => res.end())
     .pipe(res)
